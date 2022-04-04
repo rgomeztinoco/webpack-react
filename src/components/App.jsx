a5e1b5cf7d1545fac3a0f7a04bf160dd32daeb51
rgomeztinoco <gricardo2213@gmail.com>
2022-04-03 23:25:50 -0500

    🎨 [ADD] webpack config
    added cofig for new React proyect

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>React App!!!</h1>
+      <p>Pueba de config de webpack</p>
+      <p>hello</p>
+    </div>
+  );
+}
+
+export default App;
